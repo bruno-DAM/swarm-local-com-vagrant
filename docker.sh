b568ebf3e086f6d82c691ede57df0d2c0f70529e
bruno-DAM <brunodavilaamorim2006@gmail.com>
2025-09-09 21:49:45 -0300

    Instalação Docker

---
+++ docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Instalação do docker com versão mais atual
+sudo apt-get update -y
+curl -fsSL https://get.docker.com | sudo bash
+sudo curl -fsSL "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker vagrant
