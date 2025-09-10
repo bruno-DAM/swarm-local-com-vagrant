a0ec24e5b819f5c0aa63f7bbd87a2e2e2006afd7
bruno-DAM <brunodavilaamorim2006@gmail.com>
2025-09-09 22:22:28 -0300

    w

--- worker.sh
+++ worker.sh
@@ -5,6 +5,11 @@ MASTER_IP=$1
 # Aguardando token 
 while [ ! -f /vagrant/worker_token.txt ]; do
   sleep 4
+  COUNT=$((COUNT + 1)) 
+  if [ $COUNT -gt 15 ]; then 
+  echo "Não encontrado token após 1 minuto de espera. Abortando." 
+  exit 1 
+ fi 
 done
 
 TOKEN=$(cat /vagrant/worker_token.txt)
