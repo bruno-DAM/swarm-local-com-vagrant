d6be63f964cbf4fc105b6e06079d5c74df42c0fd
bruno-DAM <brunodavilaamorim2006@gmail.com>
2025-09-09 22:02:55 -0300

    MV master

--- master.sh
+++ master.sh
@@ -4,3 +4,6 @@ MASTER_IP=$1
 
 sudo docker swarm init --advertise-addr=${MASTER_IP}
 sudo docker swarm join-token worker -q > /vagrant/worker_token.txt
+
+# Teste para validar 
+docker service create --name teste --replicas 3 -p 8080:80 nginx
